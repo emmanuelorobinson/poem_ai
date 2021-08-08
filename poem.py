8a9387e9924c71c51b1346014ae88db4fe576760
Emmanuel Robinson <52470812+emmanuelorobinson@users.noreply.github.com>
2021-08-08 10:56:52 +0400

    Update poem.py

--- poem.py
+++ poem.py
@@ -17,4 +17,19 @@ tokenizer.fit_on_text(text)
 sequences = tokenizer.text_to_sequences(sentences)
 
 #padding sequence of text
-paddedsequence = 
+max_sequence_len = max([len(x) for x in sequences])
+paddedsequence = np.array(pad_sequences(sequences, maxlen=max_sequence_len, padding='pre'))
+
+
+
+def build_model(vocab_size, embedding_dim, rnn_units, batch_size):
+    model = tf.keras.Sequential([
+        tf.keras.layers.Embedding(vocab_size, embedding_dim,
+                                  batch_input_shape=[batch_size, None]),
+        tf.keras.layers.GRU(rnn_units,
+                            return_sequences=True,
+                            stateful=True,
+                            recurrent_initializer='glorot_uniform'),
+        tf.keras.layers.Dense(vocab_size)
+    ])
+    return model
